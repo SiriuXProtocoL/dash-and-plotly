eb49e5b65a1f57a6e1ae86ebb307cb91f25132f8
SiriuXProtocoL <bebinkrajusalkara@gmail.com>
2021-01-08 11:10:50 +0530

    2nd commit qwerty

---
+++ 01_dash_intro.py
@@ -0,0 +1,6 @@
+#/usr/bin/python3 -m pip install dash dash-renderer dash-html-components dash-core-components 
+import dash
+import dash_core_components
+import dash_html_components
+
+print("hello world")
