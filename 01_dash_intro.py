4fc6c1a6b45fa82d47b2e4ea4cfc7f81b4b891a6
SiriuXProtocoL <bebinkrajusalkara@gmail.com>
2021-01-08 12:06:26 +0530

    finaly commit for basic dash app.

--- 01_dash_intro.py
+++ 01_dash_intro.py
@@ -1,6 +1,23 @@
 #/usr/bin/python3 -m pip install dash dash-renderer dash-html-components dash-core-components 
 import dash
-import dash_core_components
-import dash_html_components
+import dash_core_components as dcc
+import dash_html_components as html
 
-print("hello world")
+app = dash.Dash()
+
+
+app.layout = html.Div(children = [
+    html.H1("Dash App"),
+    dcc.Graph(id = 'example',
+                figure = {'data' : [
+                    {'x' : [1,2,3,4,5], 'y' : [5,6,7,2,1], 'type' : 'line', 'name' : "boats"},
+                    {'x' : [1,2,3,4,5], 'y' : [8,3,2,3,5], 'type' : 'bar', 'name' : "cars"},
+                    ], 'layout' : {'title' : 'Dash Demo'}
+                })
+    ])
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
+
+
+#kill -9 $(ps -A | grep python | awk '{print $1}')
