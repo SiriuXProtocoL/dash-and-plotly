f2a9182f9116b9c3c42675bf5937fd6f90b1bfa2
SiriuXProtocoL <bebinkrajusalkara@gmail.com>
2021-01-10 12:28:33 +0530

    Simple live graph plot

---
+++ 05_live_graph.py
@@ -0,0 +1,42 @@
+import dash 
+from dash.dependencies import Input, Output
+import dash_core_components as dcc 
+import dash_html_components as html
+import plotly
+import random
+import plotly.graph_objs as go 
+from collections import deque
+#deque is a container to specify max size
+
+X = deque(maxlen=20)
+Y = deque(maxlen=20)
+X.append(1)
+Y.append(1)
+
+app = dash.Dash(__name__)
+app.layout = html.Div([
+    dcc.Graph(id='live-graph', animate=True),
+    dcc.Interval(id='graph-update', interval=100)
+])
+
+@app.callback(Output('live-graph', 'figure'),
+                [Input('graph-update', 'n_intervals')])
+
+def update_graph(input_data):
+    global X
+    global Y
+    X.append(X[-1]+1)
+    Y.append(Y[-1]+Y[-1]*random.uniform(-0.1,0.1))
+    data = go.Scatter(
+        x = list(X),
+        y = list(Y),
+        name='Scatter',
+        mode='lines+markers'
+    )
+    return {'data': [data], 
+    'layout' : go.Layout(xaxis= dict(range=[min(X),max(X)]),
+     yaxis= dict(range=[min(Y),max(Y)]))}
+
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
